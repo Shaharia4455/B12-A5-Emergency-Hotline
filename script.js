9e98c22c39a3e3a3293f325b234b9856436dc639
Shaharia4455 <mdshaharia4455@gmail.com>
2026-01-14 17:58:06 +0600

    navber, banner, heart-reaction done

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+// heart-icon
+
+const heartCountEl = document.getElementById("heart-num") ;
+let heartCount = 0 ;
+const heartCounts = document.querySelectorAll(".heart-icon") ;
+
+heartCounts.forEach((icon) => {
+    icon.addEventListener("click", () => {
+
+    icon.style.transition = "transform 0.2s ease";
+    icon.style.transform = "scale(1.5)";
+    setTimeout(() => {
+      icon.style.transform = "scale(1)";
+    }, 200);
+
+        if(icon.classList.contains("liked")){
+            heartCount--;
+            if(heartCount < 0) heartCount = 0;
+            heartCountEl.innerText = heartCount;
+
+            icon.classList.remove("fa-solid", "liked") ;
+            icon.classList.add("fa-regular") ;
+            icon.style.color = "" ;
+
+        }
+        else {
+            heartCount++ ;
+            heartCountEl.innerText = heartCount;
+
+            icon.classList.remove("fa-regular") ;
+            icon.classList.add("fa-solid", "liked") ;
+            icon.style.color = "red" ;
+        }
+})
+})
+
+// all copy button click to copy
+
+const copyButtons = document.querySelectorAll(".copy-btn");
+copyButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const card = btn.closest(".cards");
+        const numberEl = card.querySelector(".number");
+        const number = numberEl.innerText
+
+        navigator.clipboard.writeText(number)
+        .then(() => {
+            alert(`Copied : ${number} `);
+        })
+        .catch(() => {
+            alert("Failed to copy")
+        });
+    });
+});
